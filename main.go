66bcaab6cd57cf07c6d66eec71b61c1bec30a4f9
Maxim <papka.yakov@gmail.com>
2024-03-15 02:47:57 +0700

    feature: add basic http server

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
 
-import "fmt"
+func handleGET(w http.ResponseWriter, r *http.Request) {
+    name := r.URL.Query().Get("name")
+    fmt.Fprintf(w, "Hello, %s!", name)
+}
 
+func handlePOST(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseForm()
+    if err != nil {
+        http.Error(w, "Failed to parse form", http.StatusBadRequest)
+        return
+    }
+    name := r.FormValue("name")
+    fmt.Fprintf(w, "Hello, %s!", name)
+}
 
 func main() {
-    fmt.Println("hello world")
+
+	http.HandleFunc("/handle-get", handleGET)
+    http.HandleFunc("/handle-post", handlePOST)
+
+	http.ListenAndServe(":80", nil)
 }
