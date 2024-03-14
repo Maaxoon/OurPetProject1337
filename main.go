6e4e4490acd82b3f5ec9902705d53dd1a1b77aec
Maxim <papka.yakov@gmail.com>
2024-03-15 03:06:26 +0700

    fix: main.go gix

--- main.go
+++ main.go
@@ -5,31 +5,29 @@ import (
 	"net/http"
 )
 
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", "LOH")
+}
+
 func handleGET(w http.ResponseWriter, r *http.Request) {
-    name := r.URL.Query().Get("name")
-    fmt.Fprintf(w, "Hello, %s!", name)
+	name := r.URL.Query().Get("name")
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
 
 func handlePOST(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil {
-        http.Error(w, "Failed to parse form", http.StatusBadRequest)
-        return
-    }
-    name := r.FormValue("name")
-    fmt.Fprintf(w, "Hello, %s!", name)
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
+	name := r.FormValue("name")
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
 
 func main() {
-
+	http.HandleFunc("/index", handleIndex)
 	http.HandleFunc("/handle-get", handleGET)
-    http.HandleFunc("/handle-post", handlePOST)
+	http.HandleFunc("/handle-post", handlePOST)
 
 	http.ListenAndServe(":80", nil)
-=======
-import "fmt"
-
-func main() {
-	fmt.Println("hello world")
->>>>>>> 3ece21d0ff182f61ad670d8da6ffdfae3eb3adc1
 }
